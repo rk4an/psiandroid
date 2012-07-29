1894c3caa7f6cde1eb25904e3012ffa4ff602f39
Erkan Valentin <erkan2005@gmail.com>
2012-07-29 10:05:38 +0200

    get plugins with xml

--- src/com/phpsysinfo/activity/PSIConfig.java
+++ src/com/phpsysinfo/activity/PSIConfig.java
@@ -2,7 +2,7 @@ package com.phpsysinfo.activity;
 
 public class PSIConfig {
 
-	public final static String SCRIPT_NAME = "/xml.php";
+	public final static String SCRIPT_NAME = "/xml.php?plugin=complete";
 	public final static String HOSTS_JSON_STORE = "HOSTS_JSON_STORE";
 	public final static String JSON_CURRENT_HOST = "JSON_CURRENT_HOST";
 	public final static int MEMORY_SOFT_THR = 80;
